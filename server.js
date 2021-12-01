7dbf6faf98f1122e8f228ec0222d42225f586b3e
Walteann <walteann3@gmail.com>
2021-12-01 19:58:30 -0300

    feat: api proxy uma palinha

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+const API_URL = 'https://itunes.apple.com';
+
+const axios = require('axios');
+
+app.get('/api/v1/musics', async (request, response) => {
+
+    try {
+
+        const params = request.query;
+
+        const { data } = await axios(`${API_URL}/search`, { params });
+
+        return response.status(200).json(data);
+        
+    } catch (error) {
+        return response.status(error.status).json(error);
+    }
+
+});
+
+
+app.listen(port, () => {
+
+    console.log('Api está rodando');
+
+});
